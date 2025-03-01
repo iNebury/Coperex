ea33f719e583c8228f62382f96ae990c6e472956
iNebury <diegourias37@gmail.com>
2025-02-28 18:51:25 -0600

    se agrego ususarios y auth

---
+++ src/middlewares/usuario-validator.js
@@ -0,0 +1,33 @@
+import { body, param  } from "express-validator";
+import { emailExists, usernameExists, userExists } from "../helpers/db-validators.js";
+import { validarCampos } from "./validate-fields.js";
+import { handleErrors } from "./handle-errors.js";
+import { validateJWT } from "./validate-jwt.js";
+
+export const registerValidator = [
+    body("name").notEmpty().withMessage("El nombre es requerido"),
+    body("username").notEmpty().withMessage("El username es requerido"),
+    body("email").notEmpty().withMessage("El email es requerido"),
+    body("email").isEmail().withMessage("No es un email válido"),
+    body("email").custom(emailExists),
+    body("username").custom(usernameExists),
+    validarCampos,
+    handleErrors
+]
+
+export const loginValidator = [
+    body("email").optional().isEmail().withMessage("No es un email válido"),
+    body("username").optional().isString().withMessage("Username es en formáto erróneo"),
+    body("password").isLength({min: 4}).withMessage("El password debe contener al menos 8 caracteres"),
+    validarCampos,
+    handleErrors
+]
+
+export const updateUserValidator = [
+    validateJWT,
+    param("uid", "No es un ID válido").isMongoId(),
+    param("uid").custom(userExists),
+    validarCampos,
+    handleErrors
+]
+
